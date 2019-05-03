d3583319058a8e0269fc3ee348377dcd3d391f22
Manish <manish0338@gmail.com>
2019-05-03 14:43:14 -0700

    added user APIs

--- backend/OpenHack/src/main/java/com/app/OpenHack/Controller/UserController.java
+++ backend/OpenHack/src/main/java/com/app/OpenHack/Controller/UserController.java
@@ -2,15 +2,53 @@ package com.app.OpenHack.Controller;
 
 import java.security.Principal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.app.OpenHack.Controller.repository.UserRepository;
+import com.app.OpenHack.entity.User;
 
 @RestController
 public class UserController {
 	
-	@GetMapping("/test")
-	public Principal testUser(Principal principal) {
-		
-		return principal;
+	@Autowired
+	UserRepository userRepository;
+	
+	@Autowired
+	UserService userService;
+	
+	@GetMapping("/echo")
+	public Principal testUser(Authentication authentication) {
+		System.out.println(authentication.getPrincipal());
+		System.out.println(((User)authentication.getPrincipal()).getEmail());
+		return authentication;
+	}
+	
+	@PostMapping("/user")
+	@ResponseStatus(value = HttpStatus.CREATED)
+	public void createUser(@RequestBody User user) {
+		userRepository.save(user);
+	}
+	
+	@DeleteMapping("/user/{uid}")
+	@ResponseStatus(value = HttpStatus.OK)
+	public void deleteUser(@PathVariable String uid) {
+		userRepository.deleteById(uid);
+	}
+	
+	@PutMapping("/user")
+	@ResponseStatus(value = HttpStatus.OK)
+	public void updateUser(@RequestBody User user,Authentication authentication) {
+		User loggedInUser = (User)authentication.getPrincipal();
+		userService.updateUser(user, loggedInUser.getUuid());
 	}
 }
---
+++ backend/OpenHack/src/main/java/com/app/OpenHack/Controller/UserService.java
@@ -0,0 +1,32 @@
+package com.app.OpenHack.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.app.OpenHack.Controller.repository.UserRepository;
+import com.app.OpenHack.entity.User;
+
+@Service
+public class UserService {
+
+	@Autowired
+	UserRepository userRepository;
+	
+	public void updateUser(User user, String uid) {
+		User value = userRepository.findById(uid).get();
+		if(user.getName()!=null)
+			value.setName(user.getName());
+		if(user.getAboutMe()!=null)
+			value.setAboutMe(user.getAboutMe());
+		if(user.getAddress()!=null)
+			value.setAddress(user.getAddress());
+		if(user.getBussinessTitle()!=null)
+			value.setBussinessTitle(user.getBussinessTitle());
+		if(user.getOrganization()!=null)
+			value.setOrganization(user.getOrganization());
+		if(user.getPhotoUrl()!=null)
+			value.setPhotoUrl(user.getPhotoUrl());
+		if(user.getScreenName()!=null)
+			value.setScreenName(user.getScreenName());
+	}
+}
