70314ba374938997a82d407ed12b65d24b54c6bf
Ruchika Hazariwal <ruchikahazariwal17@gmail.com>
2019-05-02 18:31:41 -0700

    sign and login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Main from "./components/Main";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/lib/integration/react";
+import { persistor, store } from "./store";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//App Component
+class App extends Component {
+  render() {
+    return (
+      //Use Browser Router to route to different pages
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <BrowserRouter>
+            <div>
+              {/* App Component Has a Child Component called Main*/}
+              <Main />
+            </div>
+          </BrowserRouter>
+        </PersistGate>
+      </Provider>
+    );
+  }
 }
-
+//Export the App component so that it can be used in index.js
 export default App;
