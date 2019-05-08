796f2837321ef81489ecd1a5f789f56b44b9d42b
Manish <manish0338@gmail.com>
2019-05-07 21:19:52 -0700

    bug fix

--- backend/OpenHack/src/main/java/com/app/OpenHack/Controller/repository/UserRepository.java
+++ backend/OpenHack/src/main/java/com/app/OpenHack/Controller/repository/UserRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.app.OpenHack.entity.User;
@@ -12,5 +13,5 @@ import com.app.OpenHack.entity.User;
 public interface UserRepository extends JpaRepository<User, String> {
 	
 	@Query("SELECT u FROM User u WHERE u.email NOT LIKE CONCAT('%',:temp,'%')")
-	public List<User> findByEmailIgnoreCaseContaining(String temp);
+	public List<User> findByEmailIgnoreCaseContaining(@Param("temp")String temp);
 }
