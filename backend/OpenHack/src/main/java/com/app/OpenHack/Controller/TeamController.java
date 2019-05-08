f2ada231011e0fd8bc15cf2480b3711168a03485
Manish <manish0338@gmail.com>
2019-05-07 23:51:20 -0700

    hackathon submit API added

--- backend/OpenHack/src/main/java/com/app/OpenHack/Controller/TeamController.java
+++ backend/OpenHack/src/main/java/com/app/OpenHack/Controller/TeamController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -90,5 +91,13 @@ public class TeamController {
 		}
 		httpServletResponse.setHeader("Location", GlobalConst.UI_URL);
 	    httpServletResponse.setStatus(302);
+	}
+	
+	@PutMapping("/team/submit")
+	@ResponseStatus(value = HttpStatus.OK)
+	public void submitHackathon(Map<String, Object> payload) {
+		Team team = teamRepository.findById((Long)payload.get("teamId")).get();
+		team.setSubmitionUrl((String)payload.get("submitionUrl"));
+		teamRepository.save(team);
 	}
 }
