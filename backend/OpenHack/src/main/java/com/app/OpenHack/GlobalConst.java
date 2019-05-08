e9e8759d60d2da36e8bee31f5fba4940edf52773
Manish <manish0338@gmail.com>
2019-05-08 11:57:31 -0700

    changed UI url

--- backend/OpenHack/src/main/java/com/app/OpenHack/GlobalConst.java
+++ backend/OpenHack/src/main/java/com/app/OpenHack/GlobalConst.java
@@ -3,5 +3,5 @@ package com.app.OpenHack;
 public class GlobalConst {
 
 	public static String url = "http://localhost:8080/";
-	public static String UI_URL = "http://localhost:8080/";
+	public static String UI_URL = "http://localhost:3000/";
 }
