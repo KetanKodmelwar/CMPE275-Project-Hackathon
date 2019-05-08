6fadacd33295af06f8bf54bda0282e284811aac0
Manish <manish0338@gmail.com>
2019-05-08 15:01:42 -0700

    fixed bug

--- backend/OpenHack/src/main/java/com/app/OpenHack/entity/Organization.java
+++ backend/OpenHack/src/main/java/com/app/OpenHack/entity/Organization.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
@@ -23,6 +24,7 @@ public class Organization {
 	@Column
 	private String orgName;
 	@OneToOne
+	@JoinColumn
 	@JsonIgnoreProperties({"organization","judging","teams"})
 	private User orgOwner;
 	@Column
