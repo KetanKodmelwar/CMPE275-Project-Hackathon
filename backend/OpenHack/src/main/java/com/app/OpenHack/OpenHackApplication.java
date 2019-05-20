05083ee5a4e0335c236cf9f9f0d45d98f9f0eda7
Manish <manish0338@gmail.com>
2019-05-19 19:31:11 -0700

    changes

--- backend/OpenHack/src/main/java/com/app/OpenHack/OpenHackApplication.java
+++ backend/OpenHack/src/main/java/com/app/OpenHack/OpenHackApplication.java
@@ -16,7 +16,6 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 
 @SpringBootApplication
-@EnableAsync
 public class OpenHackApplication {
 
 	public static void main(String[] args) {
